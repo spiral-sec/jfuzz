00e46dc5029f434505855c0176fb1205af0b5032
winds0r <wontsleep@protonmail.com>
2022-06-10 16:11:07 +0200

    initial commit

---
+++ jfuzz/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+
+"""
+    TODO:
+    1. Connect to Bus interface (
+            in testing mode -> vcan
+            otherwise -> Vector
+        )
+
+    2. Parse .dbc file
+        -> Collect signals, messages and nodes
+
+    3. Generate random messages using the fetched messages we got
+
+    4. Shell Bus interface with generated messages
+
+    5. Log those messages in the console
+
+    6. Monitor for timeouts -> Could mean crashes
+"""
+
---
+++ scripts/enable-vcan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script should be run before development because vCAN is not necessarily
+# on by default.
+
+# Make sure the script runs with super user priviliges.
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+# Load the kernel module.
+modprobe vcan
+
+# Create the virtual CAN interface.
+ip link add dev vcan0 type vcan
+
+# Sets up CAN-FD
+sudo ip link set vcan0 mtu 72
+
+# Bring the virtual CAN interface online.
+ip link set up vcan0
+
