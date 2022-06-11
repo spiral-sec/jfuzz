1170fe0c09f825f8e0687ed456137bfe8e553c03
winds0r <wontsleep@protonmail.com>
2022-06-11 10:48:06 +0200

    (mod/core)parse multiple .dbc files and should translate cantools messages to python-can messages

--- jfuzz/core/database.py
+++ jfuzz/core/database.py
@@ -1,13 +1,47 @@
 # -*- coding: UTF-8 -*-
 
+import os
+from os import walk
+
+import can
+from cantools.database import load_file
+
 """
     This class implements the DBC parser from cantools, which sends back Messages that can be sent to the CAN bus
     with python-can
 """
-from cantools import database
-
 
 class Database:
-    def __init__(self, filepath: str) -> None:
-        self.__file = database.load_file(filepath)
-        self.messages = self.__file.messages
+    def __init__(self, project_dir_path: str) -> None:
+        self.files: [str] = []
+        self.messages : [can.Message] = []
+        self.nodes : [str] = []
+
+        self.collect_dbc_files(project_dir_path)
+        for file in self.files:
+            self.parse(file)
+
+    def collect_dbc_files(self, dir_path) -> None:
+        sep = '/' if os.environ.get('DEV', False) else '\\'
+
+        for root, _dirs, files in walk(dir_path):
+            for file in files:
+                if file.endswith('.dbc'):
+                    new_file = root + sep + file
+                    self.files.append(new_file)
+        return None
+
+
+    def parse(self, filepath: str) -> None:
+
+        """
+        :param filepath: path of .dbc file
+        :return: Nothing
+        """
+
+        if cantools_db := load_file(filepath):
+            for node in cantools_db.nodes:
+                self.nodes.append(node.name)
+
+            for msg in cantools_db.messages:
+                print(f'msg to translate -> {msg}')
